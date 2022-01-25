ca8e64ebd680cd82dd54a8f8f776832395301070
otchereErnest <ernestowusu80@gmail.com>
2022-01-25 12:18:17 +0000

    change AUTH Latest-01

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@
 Route::get('/', 'MainController@welcome');
 Route::get( '/auth0/callback', '\Auth0\Login\Auth0Controller@callback' )->name( 'auth0-callback' );
 Route::get('/login', 'Auth0IndexController@login' )->name( 'login' );
-Route::get('/logout', 'Auth0IndexController@logout' );
+Route::get('/logout', 'Auth0IndexController@logout' )->name( 'logout' );
 Route::get('/home', 'MainController@index' )->name( 'home' );
